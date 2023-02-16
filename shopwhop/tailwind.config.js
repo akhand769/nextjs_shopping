882ff5d7f1d08cec35a0821a0752974d8a0c81c6
akhand769 <akhand769@gmail.com>
2023-02-17 00:19:51 +0530

    using javascript

--- shopwhop/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./pages/**/*.{html,js,tsx,ts}', './styles/**/*.{html,js,tsx,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
